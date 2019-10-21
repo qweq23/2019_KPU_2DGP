7522dfd66bb9f5d13b96fbb600ed2abbfb5d9198
qweq23 <33680108+qweq23@users.noreply.github.com>
2019-10-21 23:05:29 +0900

    리소스(이미지) 수정

---
+++ Gallaga/Bullet.py
@@ -0,0 +1,14 @@
+from Global import *
+
+# bullet list가 있어야하는데 어디..?
+
+
+class PlayerBullet:
+    def __init__(self, init_xPos):
+        self.x, self.y = init_xPos, CONST_PLAYER_Y
+        self.image = load_image("./image/player_bullet_9.png")
+
+
+class EnemyBullet:
+    def __init__(self):
+        pass
