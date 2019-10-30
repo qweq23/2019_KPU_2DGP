a4bb52b4e13c4ba00071c3583a778ca383b20bd2
qweq23 <33680108+qweq23@users.noreply.github.com>
2019-10-31 05:02:33 +0900

    적 추가

--- Galaga/Enemy.py
+++ Galaga/Enemy.py
@@ -1,8 +1,26 @@
 from pico2d import *
 
+# 적들의 3가지 상태
+# 1. 등장 2. 정렬 3. 퇴장
+# 적이 생성될 때 어느 위치에 정렬될 지 그 좌표값을 가지고 있어야 한다.
 
 class Enemy:
-    pass
+    image = None
+
+    def __init__(self, x, y):
+        self.arrayed_x, self.arrayed_y = x, y
+        self.cur_x, self.cur_y = None, None
+        if Enemy.image == None:
+            Enemy.image = load_image('Image/enemy_sprite_37x68.png')
+
+    def update(self):
+        # 위치 업데이트
+        pass
+
+    def draw(self):
+        pass
+
+    # 적은 스테이지가 진행되는 동안 정해진 시간에 정해진 위치에 있게 된다 즉, 동선이 정해져 있다
 
 
 class Bee(Enemy):
@@ -14,4 +32,7 @@ class Butterfly(Enemy):
 
 
 class Moth(Enemy):
-    pass
+    def __init__(self, x, y):
+        Enemy.__init__(self, x, y)
+        self.hp = 2
+
