d4fdee52901d6181fe7ddd08bbcfc937606ab56f
qweq23 <33680108+qweq23@users.noreply.github.com>
2019-11-19 15:55:29 +0900

    ui data 저장구조 변경

--- Galaga/UI_manager.py
+++ Galaga/UI_manager.py
@@ -10,14 +10,10 @@ class UI_Manager:
     def __init__(self):
         self.event_que = []
 
-        self.life = 3
-        self.stage_number = 1
-        self.score = 0
-
-        self.event_table = {
-            LIFE: self.life,
-            STAGE: self.stage_number,
-            SCORE: self.score
+        self.ui_data = {
+            LIFE: 3,
+            STAGE: 1,
+            SCORE: 0
         }
 
         # self.frame = 0 필요할까?
@@ -34,21 +30,16 @@ class UI_Manager:
     def update(self):
         if len(self.event_que) > 0:
             event = self.event_que.pop()
-            if event[0] == LIFE:
-                pass
-            elif event[0] == STAGE:
-                self.stage_number += event[1]
-            elif event == SCORE:
-                pass
+            self.ui_data[event[0]] += event[1]
 
     def draw(self):
         self.frame_image.draw(700, 400)
 
         self.font.draw(600 + 20, 760, 'SCORE', (255, 0, 0))
-        self.font.draw(600 + 20, 720, '%d' % self.score, (255, 255, 255))
+        self.font.draw(600 + 20, 720, '%d' % self.ui_data[SCORE], (255, 255, 255))
 
-        for i in range(0, self.life):
+        for i in range(0, self.ui_data[LIFE]):
             self.life_image.draw(620 + 25 + (i * 55), 600, 50, 50)
 
-        self.stage_image.clip_draw((5 - self.stage_number) * 19, 0, 19, 19, 620 + 25, 400, 50, 50)
+        self.stage_image.clip_draw((5 - self.ui_data[STAGE]) * 19, 0, 19, 19, 620 + 25, 400, 50, 50)
 
